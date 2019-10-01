93892a95a23a165015db84e6b95a289ea3158e7e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-10-01 11:42:31 +0300

    из одной активити передеаем текст и читаем его в другой... играемся с методом setVisibility ... )))

--- app/src/main/java/com/omelchenkoaleks/starttechopolice/SecondMainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/starttechopolice/SecondMainActivity.java
@@ -2,6 +2,10 @@ package com.omelchenkoaleks.starttechopolice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -10,5 +14,20 @@ public class SecondMainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_main);
+
+        final EditText enterText = findViewById(R.id.editText);
+        final TextView showText = findViewById(R.id.textView3);
+
+        showText.setVisibility(View.INVISIBLE);
+
+        Button sendText = findViewById(R.id.button);
+        sendText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String receivedText = enterText.getText().toString();
+                showText.setText(receivedText);
+                showText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
