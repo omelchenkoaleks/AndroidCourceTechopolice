67df4835a6b4c7179fcb7b51b0be95e9eb39d62d
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-10-02 15:03:10 +0300

    baseline выравнивает содержимое текста по линии, но нужно учитывать размеры текста и правильно указать отношения, чтобы более крупный текст не вылез (скрылся частично) - закоментированое сравнить ...

---
+++ app/src/main/java/com/omelchenkoaleks/starttechopolice/_002_layout/layout_constraintBaseline_toBaselineOfActivity.java
@@ -0,0 +1,16 @@
+package com.omelchenkoaleks.starttechopolice._002_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omelchenkoaleks.starttechopolice.R;
+
+public class layout_constraintBaseline_toBaselineOfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_constraint_baseline_to_baseline_of);
+    }
+}
--- app/src/main/java/com/omelchenkoaleks/starttechopolice/_002_layout/layout_constraintHorizontal_biasActivity.java
+++ app/src/main/java/com/omelchenkoaleks/starttechopolice/_002_layout/layout_constraintHorizontal_biasActivity.java
@@ -2,7 +2,10 @@ package com.omelchenkoaleks.starttechopolice._002_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.omelchenkoaleks.starttechopolice.R;
 
@@ -12,5 +15,16 @@ public class layout_constraintHorizontal_biasActivity extends AppCompatActivity 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_constraint_horizontal_bias);
+
+        Button next = findViewById(R.id.next_button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        layout_constraintHorizontal_biasActivity.this,
+                        layout_constraintBaseline_toBaselineOfActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
